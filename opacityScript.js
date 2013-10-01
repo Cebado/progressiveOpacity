06d6c9ca850096994a16dcc11cbd62555feb9e44
Luis Domínguez <luisdc86@gmail.com>
2013-10-01 14:21:08 +0200

    Update opacityScript.js

--- opacityScript.js
+++ opacityScript.js
@@ -1,4 +1,3 @@
-
 $(window).scroll(function() {
     opacityScroll();
 });
@@ -9,7 +8,7 @@ function opacityScroll (){
     var topLimit = 180;
     var degradation = 320;
 
-    $('.info').each(function() {
+    $('.info').each(function() { // change .info for the name of the class that you want to have this effect
         $(this).css('opacity', 1);
         if($(this).offset().top > $(window).scrollTop() + topLimit){
             $(this).css('opacity',1-($(this).offset().top - $(window).scrollTop())/degradation);
